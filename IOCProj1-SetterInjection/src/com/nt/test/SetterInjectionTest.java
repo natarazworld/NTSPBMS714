5fbe9018cd74b19ca0041c9be42b4d15f3addd1e
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-03-29 08:25:45 +0530

    Spring First App

---
+++ IOCProj1-SetterInjection/src/com/nt/test/SetterInjectionTest.java
@@ -0,0 +1,26 @@
+//SetterInjectionTest.java
+package com.nt.test;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.nt.beans.WishMessageGenerator;
+
+public class SetterInjectionTest {
+
+	public static void main(String[] args) {
+	   //Locate and hold spring bean cfg file
+		FileSystemResource res=new FileSystemResource("src/com/nt/cfgs/applicationContext.xml");
+		//create IOC container
+		XmlBeanFactory factory=new XmlBeanFactory(res);
+		//get TargetBean class object
+		Object obj=factory.getBean("wmg");
+		//type casting
+		WishMessageGenerator generator=(WishMessageGenerator)obj;
+		//invoke the b.method
+		String result=generator.generateWishMesage("raja");
+		System.out.println("Result ::"+result);
+
+	}
+
+}
