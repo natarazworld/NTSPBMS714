6d3c8914a71f33e4f8cc1249587ecacde623acbb
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2022-01-23 09:23:29 +0530

    RefreshScope

--- SpringBootMsProj05-EmpRestService/src/main/java/com/nt/controller/EmployeeOperationsController.java
+++ SpringBootMsProj05-EmpRestService/src/main/java/com/nt/controller/EmployeeOperationsController.java
@@ -1,12 +1,14 @@
 package com.nt.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/emp")
+@RefreshScope
 public class EmployeeOperationsController {
 	
 	@Value("${dbuser}")
