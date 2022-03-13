a89e577af87a81bf9a4a62d4e7a20808e96ed2ba
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2022-03-13 09:39:22 +0530

    JWT

--- JWTProj01/src/main/java/com/nt/test/JWTTest.java
+++ JWTProj01/src/main/java/com/nt/test/JWTTest.java
@@ -2,10 +2,25 @@ package com.nt.test;
 
 import com.nt.utility.JWTUtil;
 
+import io.jsonwebtoken.Claims;
+
 public class JWTTest {
 
 	public static void main(String[] args) {
 		String token=JWTUtil.generateToken("5435AA", "RAJA", "RANI");
 		System.out.println(token);
+		//String token="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NDM1QUEiLCJzdWIiOiJSQUpBIiwiaXNzIjoiTklUIiwiaWF0IjoxNjQ3MTQzMjQ2LCJleHAiOjE2NDcxNDMzMDZ9.T0ycy47h5D_idoDob2XBggeLJfqzp_DvBXph_gSFROw";
+		System.out.println("==============================");
+		Claims claims=JWTUtil.getClaims("RANI", token);
+		/*System.out.println("subject/username ::"+claims.getSubject());
+		System.out.println(" client id ::"+claims.getId());
+		System.out.println("expirty  date ,time ::"+claims.getExpiration());
+		System.out.println("issued date ,time"+claims.getIssuedAt());*/
+		
+		System.out.println("========================");
+		System.out.println("is this  token valid ::"+JWTUtil.isTokenValid("RANI", token));
+		System.out.println(" Expiry Date and time ::"+JWTUtil.getExpiryDate("RANI", token));
+		System.out.println(" user/subject name ::"+JWTUtil.getSubject("RANI", token));
+		
 	}
 }
--- JWTProj01/src/main/java/com/nt/utility/JWTUtil.java
+++ JWTProj01/src/main/java/com/nt/utility/JWTUtil.java
@@ -1,10 +1,12 @@
 package com.nt.utility;
 
+import java.util.Base64;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.util.Base64;
 
 public class JWTUtil {
 	
@@ -17,8 +19,28 @@ public class JWTUtil {
 				     .setExpiration(new Date(System.currentTimeMillis()+TimeUnit.MINUTES.toMillis(1)))
 				     .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode(skey.getBytes()))
 				     .compact();
-				     
+	}
+	
+	public  static  Claims getClaims(String skey , String token) {
+		return  Jwts.parser().setSigningKey(Base64.getEncoder().encode(skey.getBytes()))
+				      .parseClaimsJws(token)
+				      .getBody();
 		
 	}
+	
+	public  static  boolean  isTokenValid(String skey, String token) {
+		Date tokenExpiryDate=getClaims(skey, token).getExpiration();
+		Date  sysDate=new Date();
+		return  sysDate.before(tokenExpiryDate);
+	}
+	
+	public  static   String  getSubject(String skey,String token) {
+		 return  getClaims(skey, token).getSubject();
+	}
+	
+	public static Date  getExpiryDate(String skey ,String token) {
+		 return  getClaims(skey, token).getExpiration();
+	}
+	
 
 }
